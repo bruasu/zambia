2c50bbe29e916dad5582a0f322323743fe1fd578
bruasu <bruasu@gmail.com>
2019-05-02 19:53:04 -0300

    finish part Gill

--- js/myMap.js
+++ js/myMap.js
@@ -1,13 +1,44 @@
 function initMap() {
-               var local = {lat: -13.133897, lng: 27.849332};
-               var divMap = document.getElementById('map');
-               var options = {
-                   zoom: 7,
-                   center: local,
-                   disableDefaultUI: true //disbilita controles da UI
-               };
+    //image
+    let flag = 'img/bandera.png';
+    let location = "img/location.png";
 
-               var map = new google.maps.Map(divMap, options);
+    //coordinates
+    let local = {lat: -13.133897, lng: 27.849332};
+    let capital = {lat: -15.404197, lng:28.308617};
+    let catarataVictoria = {lat: -17.924294, lng: 25.856594};
+    let nationalPark = {lat: -12.967226, lng: 31.547192};
 
-           }
-		   //-13.133897,27.849332,7z
+
+    var divMap = document.getElementById('map');
+    var options = {
+        zoom: 6,
+        center: local,
+        disableDefaultUI: true //disbilita controles da UI
+    };
+
+    var map = new google.maps.Map(divMap, options);
+
+    let imgFlag = new google.maps.Marker({
+        position: capital,
+        map: map,
+        icon: flag,
+        title: 'capital Zambia'
+    });
+
+    let imgCatarataV = new google.maps.Marker({
+        position: catarataVictoria,
+        map: map,
+        icon: location,
+        title: 'Catarata Victoria'
+    });
+
+    let imgNational = new google.maps.Marker({
+        position: nationalPark,
+        map: map,
+        icon: location,
+        title: 'national Park'
+    });
+}
+ //-13.133897,27.849332,2z
+           
